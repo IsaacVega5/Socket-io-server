4f4daec6b13935ba6bd9dcfa9bdc1ce2cb5dac35
IsaacVega <isaacvega361@gmail.com>
2023-07-16 18:54:07 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const PORT = 3000;
+
+const io = require('socket.io')(PORT,{
+    cors:{
+        origin: ['http://127.0.0.1:8000']
+    }
+})
+
+io.on('connection', socket => {
+    console.log(socket.id);
+
+    socket.on('send-msg', (msg, room) =>{
+        room === '' ? 
+            socket.broadcast.emit('share-msg', msg) : 
+            socket.to(room).emit('share-msg', msg)
+        console.log(msg);
+    });
+
+    socket.on('join-room', (room, callback) =>{
+        socket.join(room);
+        callback(`<strong style="color:blue">Just join to room:</strong> ${room}`);
+    });
+})
+
+console.log("Server ON");
